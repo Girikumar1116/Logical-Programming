25314fa2e1260a8c7bd987c374359b803bebcdee
girikumar <girikumar1116@gmail.com>
2023-05-01 10:11:51 +0530

    fibonacci series

---
+++ Fibonacci_Series.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Fibonacci_Series
+    {
+        public  void Series()
+        {
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+            int a = 0;
+            Console.WriteLine(a);
+            int b = 1;
+            Console.WriteLine(b);
+            for (int i = 2; i <= n; i++)
+            {
+                int c;
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+
+            }
+        }
+        //public  static void Main() { Fibonacci_Series fibonacci_Series = new Fibonacci_Series(); fibonacci_Series.Series();}
+    }
+}
