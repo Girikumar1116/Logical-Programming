30e1b9e63eccbe655c7ebc669cd8ca48605c311b
girikumar <girikumar1116@gmail.com>
2023-05-01 10:13:31 +0530

    perfect numbers

---
+++ Perfect_NUmbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Perfect_NUmbers
+    {
+        public void perfect_Num()
+        {
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+            int temp = num;
+            int res = 0;
+            for (int i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    res += i;
+                }
+            }
+            Console.WriteLine(res);
+            if (res == num)
+                Console.WriteLine("prefect number");
+            else
+                Console.WriteLine("Non perfect number");
+        }
+        static void Main() { Perfect_NUmbers perfect= new Perfect_NUmbers(); perfect.perfect_Num(); }
+
+    }
+}
